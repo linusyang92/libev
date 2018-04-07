efde2c9ef9cfc977b9e48e1fec45c3918fba831b
Linus Yang <laokongzi@gmail.com>
2018-04-07 21:04:27 +0800

    Force enable kqueue on macOS

--- ev.c
+++ ev.c
@@ -2760,11 +2760,11 @@ ev_recommended_backends (void) EV_THROW
 #ifndef __NetBSD__
   /* kqueue is borked on everything but netbsd apparently */
   /* it usually doesn't work correctly on anything but sockets and pipes */
-  flags &= ~EVBACKEND_KQUEUE;
+  // flags &= ~EVBACKEND_KQUEUE;
 #endif
 #ifdef __APPLE__
   /* only select works correctly on that "unix-certified" platform */
-  flags &= ~EVBACKEND_KQUEUE; /* horribly broken, even for sockets */
+  // flags &= ~EVBACKEND_KQUEUE; /* horribly broken, even for sockets */
   flags &= ~EVBACKEND_POLL;   /* poll is based on kqueue from 10.5 onwards */
 #endif
 #ifdef __FreeBSD__
